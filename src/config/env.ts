83b675c3b16039fad612293befac3b82a67717bd
Marat252552 <117304418+Marat252552@users.noreply.github.com>
2023-06-06 22:48:57 +0300

    0.3.1

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,5 @@
 
 
-export const backend_url = 'http://localhost:3000'
+export const backend_url = 'https://pastebin-back.onrender.com'
 
-export const front_url = 'http://localhost:5173'
+export const front_url = 'https://pastebin-front.onrender.com'
