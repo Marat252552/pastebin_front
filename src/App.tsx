4e62408e2642be00e7e94b5a0168b0549e8e1bf6
Marat252552 <117304418+Marat252552@users.noreply.github.com>
2023-06-07 14:12:15 +0300

    0.3.3

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate, HashRouter } from "react-router-dom"
 import MainPage from "./components/Pages/MainPage"
 import ViewPage from "./components/Pages/ViewPage"
 import NotFoundPage from "./components/Pages/NotFoundPage"
@@ -7,15 +7,15 @@ import CompletedPinPage from "./components/Pages/CompletedPinPage"
 
 
 const App = () => {
-  return <BrowserRouter>
+  return <HashRouter>
     <Routes>
-      <Route path='/#/view/:pin_id' element={<ViewPage />} />
-      <Route path='/#/create' element={<MainPage />} />
-      <Route path='/#/notfound' element={<NotFoundPage />} />
-      <Route path='/#/completed/:pin_id' element={<CompletedPinPage />} />
-      <Route path='/#/*' element={<Navigate to='/#/create' />} />
+      <Route path='/view/:pin_id' element={<ViewPage />} />
+      <Route path='/create' element={<MainPage />} />
+      <Route path='/notfound' element={<NotFoundPage />} />
+      <Route path='/completed/:pin_id' element={<CompletedPinPage />} />
+      <Route path='*' element={<Navigate to='/create' />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 }
 
 export default App
