6152d9151b4dd78f73969bbf97b7a4908ebaedae
Marat252552 <117304418+Marat252552@users.noreply.github.com>
2023-06-08 20:21:25 +0300

    0.9

--- src/components/Pages/MainPage/index.tsx
+++ src/components/Pages/MainPage/index.tsx
@@ -7,7 +7,6 @@ import { useEffect, useState } from 'react'
 import { v4 } from "uuid"
 import { useNavigate } from 'react-router-dom'
 import SwitchField from "./components/SwitchField"
-// import ModalWindow from "./components/ModalWindow"
 import PageMainTemplate from "../../Shared/Templates/Pages/MainTemplate"
 import NameField from "./components/NameField"
 import MuiTextField from "./components/MuiTextField"
@@ -63,19 +62,19 @@ const MainPage = () => {
         });
     };
 
-    // const callSuccess = (value: string) => {
-    //     messageApi.open({
-    //         type: 'success',
-    //         content: value,
-    //     });
-    // };
+    const callSuccess = (value: string) => {
+        messageApi.open({
+            type: 'success',
+            content: value,
+        });
+    };
 
-    // const callInfo = (value: string) => {
-    //     messageApi.open({
-    //         type: 'warning',
-    //         content: value,
-    //     });
-    // };
+    const callInfo = (value: string) => {
+        messageApi.open({
+            type: 'warning',
+            content: value,
+        });
+    };
 
     return <>
         <PageMainTemplate>
@@ -102,9 +101,6 @@ const MainPage = () => {
                             <SwitchField
                                 register={register}
                             />
-
-                            {/* <ModalWindow callSuccess={callSuccess} callInfo={callInfo} /> */}
-
 
                             <Uploader
                                 getValues={getValues}
--- src/config/env.ts
+++ src/config/env.ts
@@ -3,5 +3,5 @@
 export const backend_url = 'https://pastebin-back.onrender.com'
 // export const backend_url = 'http://localhost:3000'
 
-export const front_url = 'https://pastebin-front.onrender.com'
+export const front_url = 'https://simple-pastebin.onrender.com'
 // export const front_url = 'http://localhost:5173'
